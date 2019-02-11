8ba3190cf4639dc944371352a57ee27e51a82653
Anastasiya Maltsava <anastasiya.maltsava@LeverX.local>
2019-02-11 16:35:01 +0300

    hw5

--- NewApp/service/server.js
+++ NewApp/service/server.js
@@ -7,7 +7,7 @@ var xsenv = require("@sap/xsenv");
 var port  = process.env.PORT || 3000;
 
 var options = {
-  	anonymous : true, // remove to authenticate calls
+  	// anonymous : true, // remove to authenticate calls
     redirectUrl : "/index.xsjs"
     // redirectUrl : "/index.html"
 };
@@ -21,7 +21,7 @@ try {
 
 // configure UAA.  Our uaa is pt_uaa
 try {
-    options = Object.assign(options, xsenv.getServices({ uaa: "pt_uaa" }));
+    options = Object.assign(options, xsenv.getServices({ xsuaa: "pt_uaa" }));
 } catch (err) {
     console.log("[ERROR]", err.message);
 }
